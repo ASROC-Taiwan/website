0a4ec3f9e0cf044448f862cf992c51d461417286
mahopan <mahopan@users.noreply.github.com>
2026-02-05 21:09:30 +0000

    deploy: 66f1f18096de9aaa3a3f276c8889cde1f1a6b037

---
+++ uploads/minutes/misc/minutes_e.php
@@ -0,0 +1,293 @@
+
+<!doctype html>
+<html>
+<head>
+<!-- Google tag (gtag.js) -->	
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WPWGFCRZ7J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WPWGFCRZ7J');
+</script>
+
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ASROC/The Astronomical Society of the Republic of China (Taiwan)</title>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link href="_css18/master.css" rel="stylesheet" type="text/css" /></head>
+<body>
+<!-- START the outter container -->
+<div class="container">
+	<div class="row align-items-center">
+	<div class="col-auto">
+	<a href="emblem_e.php"><img src="_img18/logo_asroc_s.png" width="150" height="120" alt="會徽Logo"></a>
+	</div>
+	<div class="col-auto">
+	<a href="index_e.php"><img src="_img18/banner.png" width="570" height="85" alt="名稱Title"></a>
+	</div>
+	<div class="col-auto">
+	<a href="http://www.iau.org/" target="_blank"><img src="_img18/logo_iau.png" width="64" height="45" alt="IAU"></a>
+	</div>
+	</div><nav class="navbar sticky-top navbar-expand-lg navbar-dark rounded">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+	<ul class="navbar-nav text-md-center nav-justified w-100">
+	  <li class="nav-item">
+		<a class="nav-link" href="index_e.php">Home</a>
+	  </li>
+	  <li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="orgnization_e.php" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About ASROC</a>
+		<div class="dropdown-menu" aria-labelledby="dropdown01">
+		  <a class="dropdown-item" href="orgnization_e.php">Organization</a>
+		  <a class="dropdown-item" href="history_e.php">History</a>
+		  <a class="dropdown-item" href="rules_e.php">Regulations</a>
+		  <a class="dropdown-item" href="minutes_e.php">Minutes</a>
+		  <a class="dropdown-item" href="emblem_e.php">Emblems</a>
+		</div>
+	  </li>
+	  
+	  <li class="nav-item">
+		<a class="nav-link" href="assembly_e.php">ASROC Meetings</a>
+	  </li>
+	  <li class="nav-item">
+		<a class="nav-link" href="award_e.php">Awards</a>
+	  </li>
+	  <li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="join_e.php" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Membership</a>
+		<div class="dropdown-menu" aria-labelledby="dropdown02">
+		  <a class="dropdown-item" href="join_e.php">Application</a>
+		  <a class="dropdown-item" href="groupmember_e.php">Group Members</a>
+		</div>
+	  </li>
+	  
+	  <li class="nav-item">
+		<a class="nav-link" href="publication_e.php">Publications</a>
+	  </li>
+	  <li class="nav-item">
+		<a class="nav-link" href="link_e.php">Links</a>
+	  </li>
+	  <li class="nav-item">
+		<a class="nav-link" href="minutes.php"><i class="fa fa-language" aria-hidden="true"></i> 正體中文</a>
+	  </li>
+	</ul>
+  </div>
+</nav>
+
+<hr><!----- ----->
+
+<h2>Minutes</h2>
+
+<div class="row">
+<!-- START row of columns -->
+  <div class="col-sm">
+    <div class="card border-primary">
+    <div class="card-header text-white bg-primary">Minutes for General Assemblies</div>
+    <div class="card-body">
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_21May2023.pdf" target="_blank">2023</a>		
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_02Oct2022.pdf" target="_blank">2022</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_04Sep2020.pdf" target="_blank">2020</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_19May2019.pdf" target="_blank">2019</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_20May2018.pdf" target="_blank">2018</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_21May2017.pdf" target="_blank">2017</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_15May2016.pdf" target="_blank">2016</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_24May2015.pdf" target="_blank">2015</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_25May2014.pdf" target="_blank">2014</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_26May2013.pdf" target="_blank">2013</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_27May2012.pdf" target="_blank">2012</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_28May2011.pdf" target="_blank">2011</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_29May2010.pdf" target="_blank">2010</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_23May2009.pdf" target="_blank">2009</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_31May2008.pdf" target="_blank">2008</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_24Apr2004.pdf" target="_blank">2004</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_19Apr2003.pdf" target="_blank">2003</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_20Apr2002.pdf" target="_blank">2002</a>
+    <a class="btn btn-outline-primary  mb-1" role="button" href="record/minutes/ASROC_19May2001.pdf" target="_blank">2001</a>
+    </div>
+    </div>
+</div>
+
+<div class="col-sm">
+    <div class="card border-success">
+    <div class="card-header text-white bg-success">Minutes for Trustee and Audit Meetings</div>
+    <div class="card-body">
+
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2024">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20240510.pdf" target="_blank">2024-05-10</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20241004.pdf" target="_blank">10-04</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20241213.pdf" target="_blank">12-13</a>
+    </div> 
+
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2023">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20230320.pdf" target="_blank">2023-03-20</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20230818.pdf" target="_blank">08-18</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20231006.pdf" target="_blank">10-06</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20231208.pdf" target="_blank">12-08</a>
+    </div> 
+		
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2022">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20220318.pdf" target="_blank">2022-03-18</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20220719.pdf" target="_blank">07-19</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20220916.pdf" target="_blank">09-16</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20221209.pdf" target="_blank">12-09</a>
+    </div> 
+		
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2021">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20210420.pdf" target="_blank">2021-04-20</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20210720.pdf" target="_blank">07-20</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20210929.pdf" target="_blank">09-29</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_2021201.pdf" target="_blank">12-01</a>		
+    </div> 
+		
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2020"> 
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20200322.pdf" target="_blank">2020-03-22</a> 
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20200623.pdf" target="_blank">06-23</a> 
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20200920.pdf" target="_blank">09-20</a> 
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20201123.pdf" target="_blank">11-23</a> 
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20201229.pdf" target="_blank">12-29</a> 
+    </div>    
+
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2019">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20190427.pdf" target="_blank">2019-04-27</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20190822.pdf" target="_blank">08-22</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_201901119.pdf" target="_blank">11-19</a>
+    </div>    
+		
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2018">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20180125.pdf" target="_blank">2018-01-25</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20180224.pdf" target="_blank">02-24</a>
+  <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20180505.pdf" target="_blank">05-05</a>
+  <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20180816.pdf" target="_blank">08-16</a>
+  <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20181214.pdf" target="_blank">12-14</a>    
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2017">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20170118.pdf" target="_blank">2017-01-18</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20170414.pdf" target="_blank">04-14</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20170707.pdf" target="_blank">07-07</a> 
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20170916.pdf" target="_blank">09-16</a>    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20171202.pdf" target="_blank">12-02</a> 
+ 
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2016">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20160109.pdf" target="_blank">2016-01-09</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20160409.pdf" target="_blank">04-09</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20160726.pdf" target="_blank">07-26</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20160910.pdf" target="_blank">09-10</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20161120.pdf" target="_blank">11-20</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2015">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20150131.pdf" target="_blank">2015-01-31</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20150327.pdf" target="_blank">03-27</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20150718.pdf" target="_blank">07-18</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20151003.pdf" target="_blank">10-03</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2014">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20140117.pdf" target="_blank">2014-01-17</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20140503.pdf" target="_blank">05-03</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20140617.pdf" target="_blank">06-17</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20140920.pdf" target="_blank">09-20</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes_20141213.pdf" target="_blank">12-13</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2013">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20130119.pdf" target="_blank">2013-01-19</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20130504.pdf" target="_blank">05-04</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20130914.pdf" target="_blank">09-14</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20131116.pdf" target="_blank">11-16</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2012">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20120310.pdf" target="_blank">2012-03-10</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20120505.pdf" target="_blank">05-05</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20120915.pdf" target="_blank">09-15</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20121124.pdf" target="_blank">11-24</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2011">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20110305.pdf" target="_blank">2011-03-05</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20110618.pdf" target="_blank">06-18</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20111001.pdf" target="_blank">10-01</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/minutes20111217.pdf" target="_blank">12-17</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2010">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20100314.pdf" target="_blank">2010-03-14</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20100705.pdf" target="_blank">07-05</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20101030.pdf" target="_blank">10-30</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2009">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20090416.pdf" target="_blank">2009-04-16</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20090616.pdf" target="_blank">06-16</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20091024.pdf" target="_blank">10-24</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2008">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20080222.pdf" target="_blank">2008-02-22</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20080514.pdf" target="_blank">05-14</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20081227.pdf" target="_blank">12-27</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2007">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20070124.pdf" target="_blank">2007-01-24</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2006">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20060125.pdf" target="_blank">2006-01-25</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20060217.pdf" target="_blank">02-17</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20060309.pdf" target="_blank">03-09</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2005">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20050618.pdf" target="_blank">2005-06-18</a>
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20051203.pdf" target="_blank">12-03</a>
+    </div>
+    <div class="btn-group btn-group-sm mb-1" role="group" aria-label="2004">
+    <a class="btn btn-outline-success px-1" role="button" href="record/minutes/record20041210.pdf" target="_blank">2004-12-10</a>
+    </div>
+    </div>
+    </div>
+</div>
+
+<div class="col-sm">
+    <div class="card border-info">
+    <div class="card-header text-white bg-info">Minutes for Other Meetings</div>
+    <div class="card-body">
+    <a class="btn btn-outline-info" role="button" href="record/minutes/record20090228.pdf" target="_blank">2009-02-28</a>
+    </div>
+    </div>
+</div>  
+<!-- /END row of columns -->
+</div>
+
+    <footer>
+    <address class="small">
+    <strong>Attn: The Astronomical Society of the Republic of China (Taiwan)</strong><br>
+    General Building II, NTHU,<br>
+    No. 101, Section 2, Kuang-Fu Road, Hsinchu 30013, Taiwan<br>
+    Email: <span class="text-nowrap">secretariat</span><img src="_img18/icon_at_footer.png" width="20" height="20" alt="_replace2@_" />asroc.org.tw
+    </address>
+    </footer>
+	<button id="bk2top" type="button" class="btn btn-info" onclick="topFunction()"><i class="fa fa-angle-up fa-lg" aria-hidden="true"></i></button>
+</div> 
+<!-- /END the outter container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="_js18/jquery-3.2.1.slim.min.js"></script>
+    <script src="_js18/popper.min.js"></script>
+    <script src="_js18/bootstrap.min.js"></script>
+
+	<script>
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("bk2top").style.display = "block";
+        } else {
+            document.getElementById("bk2top").style.display = "none";
+        }
+    }
+    
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+</script>
+
+</body>
+</html>
